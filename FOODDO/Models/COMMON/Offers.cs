3c1c6325978e17a47f167d555f07a11142dab78a
harinaveen01 <harinaveen01@gmail.com>
2019-10-12 17:16:27 -0700

    Offer Class Added

---
+++ FOODDO/Models/COMMON/Offers.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FOODDO.Models.COMMON
+{
+    public class Offers
+    {
+        public int TitileId { get; set; }
+        public string Title { get; set; }
+        public string Discription { get; set; }
+        public static List<Offers> List = new List<Offers>();
+        public System.Int64 Save()
+        {
+            System.Data.SqlClient.SqlCommand cmd = null;
+            DBCon Obj = new DBCon();
+            try
+            {
+                if (this.TitileId == 0)
+                    cmd = new System.Data.SqlClient.SqlCommand("INSERT INTO Offer VALUES (@Title,@description);select SCOPE_IDENTITY();", Obj.Con);
+                else
+                {
+                    cmd = new System.Data.SqlClient.SqlCommand("UPDATE Offer SET Title=@Title,description=@description where OfferID=@OfferID", Obj.Con);
+                    cmd.Parameters.AddWithValue("@OfferID", this.TitileId);
+                }
+                cmd.Parameters.AddWithValue("@Title", this.TitileId);
+                cmd.Parameters.AddWithValue("@description", this.Discription);
+                
+               
+                if (this.TitileId == 0)
+                {
+                    this.TitileId = System.Convert.ToInt32(cmd.ExecuteScalar());
+                    if (this.TitileId > 0)
+                        Offers.List.Insert(0, this);
+                }
+                else
+                {
+                    if (cmd.ExecuteNonQuery() > 0)
+                    {
+                        Offers.List.RemoveAll(x => x.TitileId == this.TitileId);
+                        Offers.List.Insert(0, this);
+                    }
+                }
+            }
+            catch (System.Exception e) { this.TitileId = 0; e.ToString(); }
+            finally { cmd.Dispose(); Obj.Con.Close(); Obj.Con.Dispose(); Obj.Con = null; }
+            return this.TitileId;
+        }
+
+        public System.Collections.Generic.List<Offers> GetAll()
+        {
+            System.Data.SqlClient.SqlCommand cmd = null;
+            System.Data.SqlClient.SqlDataReader SDR = null;
+            System.Collections.Generic.List<Offers> ListTmp = new System.Collections.Generic.List<Offers>();
+
+            DBCon Obj = new DBCon();
+            try
+            {
+                string Query = "SELECT * FROM Offer";
+                cmd = new System.Data.SqlClient.SqlCommand(Query, Obj.Con);
+                SDR = cmd.ExecuteReader();
+                while (SDR.Read())
+                {
+                    Offers ObjTmp = new Offers();
+                    ObjTmp.TitileId = System.Int32.Parse(SDR["OfferID"].ToString());
+                    ObjTmp.Title = SDR["Title"].ToString();
+                    ObjTmp.Discription = SDR["description"].ToString();
+                    ListTmp.Add(ObjTmp);
+                }
+            }
+            catch (System.Exception e) { e.ToString(); }
+            finally { cmd.Dispose(); SDR.Close(); Obj.Con.Close(); Obj.Con.Dispose(); Obj.Con = null; }
+            return (ListTmp);
+        }
+    }
+}
