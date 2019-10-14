b46895ee9cc33137a27e146a365fd71470b07aa2
harinaveen01 <harinaveen01@gmail.com>
2019-10-14 11:38:20 -0700

    Create Edit Index  Cshtml Updated in Offer Module

---
+++ FOODDO/Controllers/OfferController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FOODDO.Models.COMMON;
+
+namespace FOODDO.Controllers
+{
+    public class OfferController : Controller
+    {
+        // GET: Offer
+        public ActionResult Index()
+        {
+            List<Offers> OfferList = Offers.List;
+            return View(OfferList);
+        }
+
+        public ActionResult Create( int ID)
+        {
+            Offers Obj = new Offers();
+            if (ID > 0)
+            {
+                Obj = Offers.List.Find(x => x.TitileId == ID);
+            }
+            return View(Obj);
+        }
+        public ActionResult CreatePost(Offers offers)
+        {
+            if(offers.Title==null || offers.Title.Replace(" ", "").Equals(""))
+            {
+                return Json(new {msg="Please Enter Title First"});
+            }
+            if(offers.Discription==null|| offers.Discription.Replace(" ", "").Equals(""))
+            {
+                return Json(new { msg = "Please Enter Discriptions " });
+            }
+            if (offers.Save() <= 0)
+            {
+                return Json(new { msg = "Can't Save Offer.. " });
+            }
+
+            return RedirectToAction("index");
+        }
+
+
+    }
+}
