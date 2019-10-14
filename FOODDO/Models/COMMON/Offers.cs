c9bb2f81428615ec5aa0f451a59d0df8664dfb89
harinaveen01 <harinaveen01@gmail.com>
2019-10-14 16:43:00 -0700

    Offer Table  Modify

--- FOODDO/Models/COMMON/Offers.cs
+++ FOODDO/Models/COMMON/Offers.cs
@@ -10,6 +10,9 @@ namespace FOODDO.Models.COMMON
         public int TitileId { get; set; }
         public string Title { get; set; }
         public string Discription { get; set; }
+        public double FromAmount { get; set; }
+        public double ToAmount { get; set; }
+        public double Bonus { get; set; }
         public static List<Offers> List = new List<Offers>();
         public System.Int64 Save()
         {
@@ -18,15 +21,17 @@ namespace FOODDO.Models.COMMON
             try
             {
                 if (this.TitileId == 0)
-                    cmd = new System.Data.SqlClient.SqlCommand("INSERT INTO Offer VALUES (@Title,@description);select SCOPE_IDENTITY();", Obj.Con);
+                    cmd = new System.Data.SqlClient.SqlCommand("INSERT INTO Offer VALUES (@Title,@description,@FromAmount,@ToAmount,@Bonus);select SCOPE_IDENTITY();", Obj.Con);
                 else
                 {
-                    cmd = new System.Data.SqlClient.SqlCommand("UPDATE Offer SET Title=@Title,description=@description where OfferID=@OfferID", Obj.Con);
+                    cmd = new System.Data.SqlClient.SqlCommand("UPDATE Offer SET Title=@Title,description=@description,FromAmount=@FromAmount,ToAmount=@ToAmount,Bonus=@Bonus where OfferID=@OfferID", Obj.Con);
                     cmd.Parameters.AddWithValue("@OfferID", this.TitileId);
                 }
                 cmd.Parameters.AddWithValue("@Title", this.Title);
                 cmd.Parameters.AddWithValue("@description", this.Discription);
-                
+                cmd.Parameters.AddWithValue("@FromAmount", this.FromAmount);
+                cmd.Parameters.AddWithValue("@ToAmount", this.ToAmount);
+                cmd.Parameters.AddWithValue("@Bonus", this.Bonus);
                
                 if (this.TitileId == 0)
                 {
@@ -66,6 +71,9 @@ namespace FOODDO.Models.COMMON
                     ObjTmp.TitileId = System.Int32.Parse(SDR["OfferID"].ToString());
                     ObjTmp.Title = SDR["Title"].ToString();
                     ObjTmp.Discription = SDR["description"].ToString();
+                    ObjTmp.FromAmount = int.Parse(SDR["FromAmount"].ToString());
+                    ObjTmp.ToAmount = int.Parse(SDR["ToAmount"].ToString());
+                    ObjTmp.Bonus = int.Parse(SDR["Bonus"].ToString());
                     ListTmp.Add(ObjTmp);
                 }
             }
